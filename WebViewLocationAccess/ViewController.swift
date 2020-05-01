a8ad33878e411c16f5017016c80dcb05e06f0f31
Hanis <hanis@outlook.com>
2020-05-01 11:02:27 +0800

    Initial commit

---
+++ WebViewLocationAccess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewLocationAccess
+//
+//  Created by Hanis Hassim on 01/05/2020.
+//  Copyright © 2020 H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
+
